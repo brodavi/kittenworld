53e1d0378c52fadfadb233a7b632b31a74c4110b
Bro Davi <brodavi@gmail.com>
2012-04-21 23:28:39 +0900

    again.. just saving stuff

--- gol.js
+++ gol.js
@@ -15,6 +15,16 @@ A.canvas.onselectstart=function(){return false;};
 // Disable document scrolling
 document.onkeydown=function(){return event.keyCode!=38 && event.keyCode!=40 && event.keyCode!=32};
 
+// A.cleanCoords
+//
+// Helper for stupid mouse coords
+A.cleanCoords = function (e) {
+    return {
+        x: e.x - A.canvas.offsetLeft,
+        y: e.y - A.canvas.offsetTop
+    };
+};
+
 // - some listeners 
 //
 A.inputs = {};
@@ -29,14 +39,26 @@ addEventListener("keyup", function (e) {
     A.inputs[k] = false;
 }, false);
 
-addEventListener("mousedown", function(e) {
-    A.inputs.mousedown = true;
-    A.inputs.mouseup = false;
-}, false);
-
-addEventListener("mouseup", function(e) {
-    A.inputs.mouseup = true;
-    A.inputs.mousedown = false;
+addEventListener("click", function(e) {
+    var goode = A.cleanCoords(e);
+    debugger;
+    if (A.wallsAvailable === 0) {
+        alert("Sorry, you don't have any walls available. Sell a soldier to buy a wall.");
+    }
+    if (A.wallsAvailable > 0 && goode.x < A.canvas.width && goode.y < A.canvas.height) {
+        var gridcoords = {
+            i: Math.floor(goode.y / GRID),
+            j: Math.floor(goode.x / GRID)
+        };
+        if (A.world[gridcoords.i][gridcoords.j] === NULL) {
+            A.world[gridcoords.i][gridcoords.j] = WALL;
+            A.wallCount += 1;
+        } else {
+            alert("You must click on a grey (blank) spot to place a wall there.");
+        }
+        A.wallsAvailable -= 1;
+        A.world.draw();
+    }
 }, false);
 
 // - For browser compatability... 
@@ -52,11 +74,6 @@ window.requestAnimFrame = (function(){
         };
 })();
 
-// old vars
-var THE_BLACK = 0;
-var THE_WHITE = 0xFF;
-
-// new vars
 var GRID = 8;
 
 var NULL = "#333333";
@@ -65,11 +82,21 @@ var SOLDIER2 = "#0000CC";
 var FOOD = "#00FF88";
 var WALL = "#000000";
 
+A.wallsAvailable = 0;
+
 A.nullCount = 0;
 A.soldier1Count = 0;
 A.soldier2Count = 0;
 A.foodCount = 0;
 A.wallCount = 0;
+
+A.colorToLabel = {
+    "#333333": "NULL",
+    "#CC0000": "SOLDIER1",
+    "#0000CC": "SOLDIER2",
+    "#00FF88": "FOOD",
+    "#000000": "WALL"
+}
 
 A.colors = {
     0: NULL,
@@ -82,6 +109,26 @@ A.colors = {
 A.ROWS = A.canvas.height / GRID;
 A.COLS = A.canvas.width / GRID;
 A.world = {};
+
+function handleSell() {
+    var sellList = new Array();
+    for (var i = 0; i < A.ROWS; i++) {
+        for (var j = 0; j < A.COLS; j++) {
+            if (A.world[i][j] === SOLDIER1) {
+                sellList.push({"i": i, "j": j});
+            }
+        }
+    }
+    var rand = Math.floor(Math.random() * sellList.length);
+    var coords = sellList[rand];
+
+    // remove that random soldier from the field
+    A.world[coords.i][coords.j] = NULL;
+    // yay, give the player a wall to use
+    A.wallsAvailable += 1;
+    alert("you now have a wall. click on the grid to place it at any time.");
+    A.world.draw();
+};
 
 function createWorld() {
     for (var i = 0; i < A.ROWS; i++) {
@@ -111,6 +158,8 @@ function createWorld() {
             for (var j = 0; j < A.COLS; j++) {
                 A.context.fillStyle = A.world[i][j];
                 A.context.fillRect(j * GRID, i * GRID, GRID, GRID);
+                A.context.fillStyle = "#000000";
+                A.context.strokeRect(j * GRID, i * GRID, GRID, GRID);
             }
         }
         document.getElementById("nullCount").innerHTML = A.nullCount;
@@ -118,6 +167,7 @@ function createWorld() {
         document.getElementById("soldier2Count").innerHTML = A.soldier2Count;
         document.getElementById("foodCount").innerHTML = A.foodCount;
         document.getElementById("wallCount").innerHTML = A.wallCount;
+        document.getElementById("wallsAvailable").innerHTML = A.wallsAvailable;
     }
 }
 
@@ -160,6 +210,8 @@ function countNeighboring (thing, i, j) {
     }
     return cnt;
 }
+
+
 
 function applyRules(world) {
     for (var i = 0; i < A.ROWS; i++) {
@@ -219,9 +271,11 @@ function applyRules(world) {
                     if (rand < 0.1) {
                         world[i][j] = SOLDIER1;
                         A.soldier1Count += 1;
+                        A.foodCount -= 1;
                     } else if (rand < 0.2) {
                         world[i][j] = SOLDIER2;
                         A.soldier2Count += 1;
+                        A.foodCount -= 1;
                     }
                 };
             }
