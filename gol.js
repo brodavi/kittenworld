a673b172af6fd2272f0f1b047fa27d0492c8133f
Bro Davi <brodavi@gmail.com>
2012-04-21 22:04:38 +0900

    just don't want to lose my changes

--- gol.js
+++ gol.js
@@ -52,72 +52,208 @@ window.requestAnimFrame = (function(){
         };
 })();
 
+// old vars
 var THE_BLACK = 0;
 var THE_WHITE = 0xFF;
 
-function createWorld(n, world) {
-    for (var i = 0; i < n; i++) {
-        var k = parseInt(Math.random()*A.canvas.width*A.canvas.height)*4;
-        world.data[k] = THE_WHITE;
-        world.data[k+1] = THE_WHITE;
-        world.data[k+2] = THE_WHITE;
+// new vars
+var GRID = 8;
+
+var NULL = "#333333";
+var SOLDIER1 = "#CC0000";
+var SOLDIER2 = "#0000CC";
+var FOOD = "#00FF88";
+var WALL = "#000000";
+
+A.nullCount = 0;
+A.soldier1Count = 0;
+A.soldier2Count = 0;
+A.foodCount = 0;
+A.wallCount = 0;
+
+A.colors = {
+    0: NULL,
+    1: SOLDIER1,
+    2: SOLDIER2,
+    3: FOOD,
+    4: WALL
+}
+
+A.ROWS = A.canvas.height / GRID;
+A.COLS = A.canvas.width / GRID;
+A.world = {};
+
+function createWorld() {
+    for (var i = 0; i < A.ROWS; i++) {
+        A.world[i] = new Array();
+        for (var j = 0; j < A.COLS; j++) {
+            var rand = Math.random();
+            if (rand < 0.1) {
+                A.world[i].push(FOOD);
+                A.foodCount += 1;
+            } else if (rand < 0.3) {
+                A.world[i].push(WALL);
+                A.wallCount += 1;
+            } else if (rand < 0.325) {
+                A.world[i].push(SOLDIER1);
+                A.soldier1Count += 1;
+            } else if (rand < 0.35) {
+                A.world[i].push(SOLDIER2);
+                A.soldier2Count += 1;
+            } else {
+                A.world[i].push(NULL);
+                A.nullCount += 1;
+            }
+        }
+    }
+    A.world.draw = function() {
+        for (var i = 0; i < A.ROWS; i++) {
+            for (var j = 0; j < A.COLS; j++) {
+                A.context.fillStyle = A.world[i][j];
+                A.context.fillRect(j * GRID, i * GRID, GRID, GRID);
+            }
+        }
+        document.getElementById("nullCount").innerHTML = A.nullCount;
+        document.getElementById("soldier1Count").innerHTML = A.soldier1Count;
+        document.getElementById("soldier2Count").innerHTML = A.soldier2Count;
+        document.getElementById("foodCount").innerHTML = A.foodCount;
+        document.getElementById("wallCount").innerHTML = A.wallCount;
     }
 }
 
-function applyRules(world, newWorld) {
-    // highly optimized
-    var width = A.canvas.width*4;
-    var d = world.data;
-    var nd = newWorld.data;
-    var cw = A.canvas.width;
-    var ch = A.canvas.height;
-    var base = 0;
-    for (var i = 0; i < d.length/4; i++) {
-        var x = i % cw;
-        var y = parseInt(i / cw);
-        
-        var cnt = 0;
-        if (x > 0 && d[base-4] != THE_BLACK) cnt++;
-        if (x < cw-1 && d[base+4] != THE_BLACK) cnt++;
-        if (x > 0 && y > 0 && d[base-4-width] != THE_BLACK) cnt++;
-        if (y > 0 && d[base-width] != THE_BLACK) cnt++;
-        if (x < cw-1 && y > 0 && d[base+4-width] != THE_BLACK) cnt++;
-        if (x > 0 && y < ch-1 && d[base-4+width] != THE_BLACK) cnt++;
-        if (y < ch-1 && d[base+width] != THE_BLACK) cnt++;
-        if (x < cw-1 && y < ch-1 && d[base+4+width] != THE_BLACK) cnt++;
-        
-        var color = null;
-        if (d[base] == THE_BLACK && cnt == 3) {
-            color = THE_WHITE;
-        } else if (!(d[base] != THE_BLACK && (cnt == 3 || cnt == 2))) {
-            color = THE_BLACK;  
-        }
-        
-        if (color !== null) {
-            nd[base] = nd[base+1] = nd[base+2] = color;
-        }
-        
-        base += 4;
+function findNeighboring (thing, i , j) {
+    var coords = {};
+
+    if (A.world[i-1]) {
+        // the row above
+        if (A.world[i-1][j-1] && A.world[i-1][j-1] === thing) { coords.row = i - 1; coords.col = j - 1; };
+        if (A.world[i-1][j] === thing) { coords.row = i - 1; coords.col = j; };
+        if (A.world[i-1][j+1] && A.world[i-1][j+1] === thing) { coords.row = i - 1; coords.col = j + 1; };
     }
-    // dead cell with 3 neightbours - alive
-    // live cell with 2 or 3 neightbours - still alive
-    // all other cases - dead
+    if (A.world[i][j-1] && A.world[i][j-1] === thing) { coords.row = i; coords.col = j - 1; }; // the 'current' row
+    if (A.world[i][j+1] && A.world[i][j+1] === thing) { coords.row = i; coords.col = j + 1; };
+    if (A.world[i+1]) {
+        // the row below
+        if (A.world[i+1][j-1] && A.world[i+1][j-1] === thing) { coords.row = i + 1; coords.col = j - 1; };
+        if (A.world[i+1][j] === thing) { coords.row = i + 1; coords.col = j; };
+        if (A.world[i+1][j+1] && A.world[i+1][j+1] === thing) { coords.row = i + 1; coords.col = j + 1; };
+    }
+    return coords;
+}
+
+function countNeighboring (thing, i, j) {
+    var cnt = 0;
+    // count adjacent "thing"
+    if (A.world[i-1]) {
+        // the row above
+        if (A.world[i-1][j-1] && A.world[i-1][j-1] === thing) { cnt += 1 };
+        if (A.world[i-1][j] === thing) { cnt += 1 };
+        if (A.world[i-1][j+1] && A.world[i-1][j+1] === thing) { cnt += 1 };
+    }
+    if (A.world[i][j-1] && A.world[i][j-1] === thing) { cnt += 1 }; // the 'current' row
+    if (A.world[i][j+1] && A.world[i][j+1] === thing) { cnt += 1 };
+    if (A.world[i+1]) {
+        // the row below
+        if (A.world[i+1][j-1] && A.world[i+1][j-1] === thing) { cnt += 1 };
+        if (A.world[i+1][j] === thing) { cnt += 1 };
+        if (A.world[i+1][j+1] && A.world[i+1][j+1] === thing) { cnt += 1 };
+    }
+    return cnt;
+}
+
+function applyRules(world) {
+    for (var i = 0; i < A.ROWS; i++) {
+        for (var j = 0; j < A.COLS; j++) {
+            if (world[i][j] === SOLDIER1) { // if soldier1....
+                // if surrounded by soldier2s... kill the 'current' soldier1... turn him into food
+                if (countNeighboring(SOLDIER2, i, j) > 2) {
+                    world[i][j] = FOOD;
+                    A.soldier1Count -= 1;
+                    A.foodCount += 1;
+                } else if (countNeighboring(FOOD, i, j) > 0) {
+                    var coords = findNeighboring(FOOD, i, j);
+                    // find the first clockwise food and replace it with soldier
+                    world[coords.row][coords.col] = SOLDIER1;
+                    A.foodCount -= 1;
+                    // replace soldier with nothing
+                    world[i][j] = NULL;
+                } else { // nothing around! what the...
+                    var rand = Math.round(Math.random() * 2 - 1);
+                    var rand2 = Math.round(Math.random() * 2 - 1);
+                    if (world[i + rand] && world[i + rand][j + rand2]) {
+                        if (rand !== 0 && rand2 !== 0) {
+                            world[i + rand][j + rand2] = SOLDIER1;
+                            world[i][j] = NULL;
+                        }
+                    }
+                }
+            }
+            if (world[i][j] === SOLDIER2) { // if soldier2....
+                // if surrounded by soldier1s... kill the 'current' soldier1... turn him into food
+                if (countNeighboring(SOLDIER1, i, j) > 2) {
+                    world[i][j] = FOOD;
+                    A.soldier2Count -= 1;
+                    A.foodCount += 1;
+                } else if (countNeighboring(FOOD, i, j) > 0) {
+                    var coords = findNeighboring(FOOD, i, j);
+                    // find the first clockwise food and replace it with soldier
+                    world[coords.row][coords.col] = SOLDIER2;
+                    // replace soldier with nothing
+                    world[i][j] = NULL;
+                    A.foodCount -= 1;
+                } else { // nothing all over the place! what the...
+                    var rand = Math.round(Math.random() * 2 - 1);
+                    var rand2 = Math.round(Math.random() * 2 - 1);
+                    if (world[i + rand] && world[i + rand][j + rand2]) {
+                        if (rand !== 0 && rand2 !== 0) {
+                            world[i + rand][j + rand2] = SOLDIER2;
+                            world[i][j] = NULL;
+                        }
+                    }
+                }
+            }
+            if (world[i][j] === FOOD) { // if food....
+                // if surrounded by foods... randomly spawn either a soldier1 or soldier2
+                if (countNeighboring(FOOD, i, j) > 3) {
+                    var rand = Math.random();
+                    if (rand < 0.1) {
+                        world[i][j] = SOLDIER1;
+                        A.soldier1Count += 1;
+                    } else if (rand < 0.2) {
+                        world[i][j] = SOLDIER2;
+                        A.soldier2Count += 1;
+                    }
+                };
+            }
+            if (world[i][j] === 9) { // if nothing...
+                var rand = function(){Math.random()};
+                if (rand <= 0.1) {
+                    world[i][j] = SOLDIER1;
+                    A.soldier1Count += 1;
+                } else if (rand <= 0.2) {
+                    world[i][j] = SOLDIER2;
+                    A.soldier2Count += 1;
+                } else if (Math.random() <= 0.3) {
+                    world[i][j] = FOOD; // spawn some food maybe
+                    A.foodCount += 1;
+                }
+            }
+        }
+    }
 }
 
 function mainLoop() {
-    var world = A.context.getImageData(0, 0, A.canvas.width, A.canvas.height);
-    var newWorld = A.context.getImageData(0, 0, A.canvas.width, A.canvas.height);
-
     if (A.inputs.k39) {
         var start = new Date();
-        applyRules(world, newWorld);
-        A.context.putImageData(newWorld, 0, 0);
+        applyRules(A.world);
+        A.world.draw(); // to optimize, just draw AS the rules are happening... we don't need to redraw
         console.log('time delta', new Date().getTime() - start.getTime());
     };
 }
 
-var world = A.context.getImageData(0, 0, A.canvas.width, A.canvas.height);
-createWorld(parseInt(0.2*A.canvas.width*A.canvas.height), world);
-A.context.putImageData(world, 0, 0);
+// create it
+createWorld();
+// draw it
+A.world.draw();
 
 setInterval(mainLoop, 50);
